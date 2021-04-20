be5cb449078d6d46fa5c40d413f84637d76d4c75
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-04-20 11:58:11 +0800

    customize: trait translations for DAOT Labs

--- config/translations.js
+++ config/translations.js
@@ -1,5 +1,34 @@
 module.exports = {
-  // 'traits.email': {
-  //   title: 'Email',
-  // },
+  'traits.email': {
+    title: 'Email (@dao.tl)',
+    position: 1,
+  },
+  password: {
+    title: 'Password',
+    position: 2,
+  },
+  'traits.username': {
+    title: 'Username (May only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.)',
+    position: 3,
+  },
+  'traits.name': {
+    title: 'Name',
+    position: 4,
+  },
+  'traits.nickname': {
+    title: 'Nickname',
+    position: 5,
+  },
+  'traits.phone_number': {
+    title: 'Phone Number',
+    position: 6,
+  },
+  identifier: {
+    title: 'Email',
+    position: 0,
+  },
+  to_verify: {
+    title: 'Your @dao.tl email address',
+    position: 0,
+  },
 }
